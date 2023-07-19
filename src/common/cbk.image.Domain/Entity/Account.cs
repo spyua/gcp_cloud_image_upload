26fc6e8de261360c5c29cd884d8a686f68dcdf19
pal2097 <pal2097@gmail.com>
2023-07-19 21:36:16 +0800

    feat-member修改密碼驗證

--- src/common/cbk.image.Domain/Entity/Account.cs
+++ src/common/cbk.image.Domain/Entity/Account.cs
@@ -4,25 +4,13 @@
     {
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
 
-        private string _password = string.Empty;
-        public string Password
-        {
-            get => _password;
-            set
-            {
-                if (!IsValidPassword(value))
-                {
-                    throw new ArgumentException("Password does not meet complexity requirements.");
-                }
-                _password = value;
-            }
-        }
         public DateTime CreateTime { get; set; }
         public DateTime LoginTime { get; set; }
 
         // 檢查密碼是否包含大小寫字母、數字和特殊字符
-        private bool IsValidPassword(string password)
+        public bool IsValidPassword(string password)
         {
             // Implement your password complexity validation here.
             // This is just a very simple example.
